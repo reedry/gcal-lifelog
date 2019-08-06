9c2e08fc700b5cd99c669e8316161f364b825f90
reedry <r.yoshida.c@gmail.com>
2019-08-07 08:54:03 +0900

    Add functions for Properties

--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,49 @@
 import { CALENDAR_ID } from './config'
 
 const targetCalendar: string = 'Activity';
+interface Activity {
+  title: string;
+  startTime: number;
+}
 
-function getNextTask(): GoogleAppsScript.Calendar.CalendarEvent | null {
+function getNextTasks(): GoogleAppsScript.Calendar.CalendarEvent[] | null {
   let cal = CalendarApp.getDefaultCalendar();
   let now = new Date();
   let oneDayFromNow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
   let events = cal.getEvents(now, oneDayFromNow);
-  if (events == []) {
-    return null;
-  }
+  let res: any[] = [];
   for (let ev of events) {
     if(!ev.isAllDayEvent()) {
-      return ev;
+      res.push(ev);
     }
   }
-  return null;
+  if (res.length > 0) {
+    return res;
+  } else {
+    return null;
+  }
+}
+
+function getCurrentActivity(): Activity | null {
+  const properties = PropertiesService.getScriptProperties();
+  const currentActivity = properties.getProperties();
+  if (currentActivity) {
+    return JSON.parse(currentActivity);
+  } else {
+    return null;
+  }
+}
+
+function registerCurrentActivity(activity: Activity): void {
+  const properties = PropertiesService.getScriptProperties();
+  properties.setProperty('CURRENT_ACTIVITY', JSON.stringify(activity));
 }
 
 function testCalApi(): void {
-  let nextTask = getNextTask();
-  if (nextTask == null) {
+  let nextTasks = getNextTasks();
+  if (nextTasks == null) {
     Logger.log("Not Found");
   } else {
-    Logger.log(nextTask.getTitle());
+    Logger.log(nextTasks[0].getTitle());
   }
 }
