f56ae31f107ff43fc3e9d2ab7e1c2e8f91857e93
ryoshid <r.yoshida.c@gmail.com>
2019-09-17 22:10:23 +0900

    Use vuetify

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
 
 const app = new Vue({
   el: '#app',
+  vuetify: new Vuetify(),
   data: {
     activity: {
       title: '',
